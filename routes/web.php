1078198e8c14d921460df5f0ae4113ca583bd424
Clément Barbaza <clement.barbaza@gmail.com>
2023-03-10 10:45:50 +0100

    init

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/users', function () {
+    $users = DB::table('users')->get();
+    return view('users', compact('users'));
+});
