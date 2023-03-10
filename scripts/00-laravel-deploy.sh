ee41f7bd1b68aa960ba8a6966b19321c1c3d6076
Clément Barbaza <clement.barbaza@gmail.com>
2023-03-10 13:28:20 +0100

    add migrations in scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,5 +2,6 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-#echo "Running migrations..."
-#php artisan migrate --force
+echo "Running migrations..."
+php artisan migrate
+php artisan db:seed
